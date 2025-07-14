08aa553f75afb56ffb8c16d1e039755fb19a8d3d
YubinH6 <tearscara@icloud.com>
2025-07-15 00:25:51 +0900

    Update 3_1_30124_app.py

--- 3_1_30124_app.py
+++ 3_1_30124_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from datetime import datetime
 
-st.title("소비 기한 할인률 계산기")
+st.title("소비 기한 할인율 계산기")
 item_name = st.selectbox("품목 선택", ["고기", "생선", "과일", "도시락", "빵", "유제품", "가공 식품", "건강 기능 식품"])
 
 purchase_date = st.date_input("구매 날짜", datetime.today())
